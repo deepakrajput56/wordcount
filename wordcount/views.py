fa36f814d801913511eeac8e34f2886d629f20bd
deepakrajput56 <deepak.rajput56@gmail.com>
2018-07-27 13:57:45 +0530

    h1 to h2

---
+++ wordcount/views.py
@@ -0,0 +1,35 @@
+
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+
+def home(request):
+
+    return render(request, "home.html")
+
+def count(request):
+    fulltext = request.GET['fulltext']
+
+    wordlist=fulltext.split()
+
+    worddictonary={}
+
+    for word in wordlist:
+        if word in worddictonary:
+
+            worddictonary[word] += 1
+
+
+        else:
+            worddictonary[word]=1
+
+    sortedwords= sorted(worddictonary.items(), key=operator.itemgetter(1),reverse=True)
+
+    return render(request, "count.html",{'fulltext': fulltext,'wordlist': len(wordlist),'sortedwords':sortedwords})
+
+def about(request):
+    return render(request,"about.html")
+
+
+
